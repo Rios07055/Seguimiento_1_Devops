6df1c1240f8ec54789c13fe4839c62b38ecf2750
Cristian Rios <rios07055@gmail.com>
2025-08-13 18:40:59 -0500

    Se implementa en facultad_service la creación de models y database

---
+++ facultad_service/models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from database import Base
+
+class Facultad(Base):
+    __tablename__ = "facultad"
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String(255), nullable=False)
+
