4db5a5a2c736ca4fe6657592c1c12ebf867476b4
Cristian Rios <rios07055@gmail.com>
2025-08-22 17:35:43 -0500

    🚀 Se implementan metodos crud para facultad_service

---
+++ facultad_service/crud.py
@@ -0,0 +1,35 @@
+from sqlalchemy.orm import Session
+import models
+import schemas
+
+def get_all(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Facultad).offset(skip).limit(limit).all()
+
+def get(db: Session, item_id: int):
+    return db.query(models.Facultad).filter(models.Facultad.id == item_id).first()
+
+def create(db: Session, item: schemas.FacultadCreate):
+    db_item = models.Facultad(**item.dict())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+def update(db: Session, item_id: int, item: schemas.FacultadUpdate):
+    db_item = get(db, item_id)
+    if not db_item:
+        return None
+    for k, v in item.dict(exclude_unset=True).items():
+        setattr(db_item, k, v)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+def delete(db: Session, item_id: int):
+    db_item = get(db, item_id)
+    if not db_item:
+        return None
+    db.delete(db_item)
+    db.commit()
+    return db_item
