037664ef0f0eb8f63d3ebe46810ac33c860430da
Cristian Rios <rios07055@gmail.com>
2025-08-22 18:58:57 -0500

    ⬆️ Se modifica la forma de acceder a la base de datos para obtener la variante de pruebas

--- estudiante_service/database.py
+++ estudiante_service/database.py
@@ -1,12 +1,24 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.pool import StaticPool
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://fastapi:1234@127.0.0.1:3306/universidad")
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///:memory:"
+)
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False},
+        poolclass=StaticPool,
+    )
+else:
+    engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
