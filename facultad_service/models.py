32bb3306f5be1dea708839b08294712ab434f020
Cristian Rios <rios07055@gmail.com>
2025-08-24 14:52:56 -0500

    ✅ Cambios en las relaciones de los modelos

--- facultad_service/models.py
+++ facultad_service/models.py
@@ -1,8 +1,7 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
 from .database import Base
 
 class Facultad(Base):
     __tablename__ = "facultad"
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String(255), nullable=False)
-
